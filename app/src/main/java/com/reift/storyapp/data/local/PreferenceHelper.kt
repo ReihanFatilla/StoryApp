7aa0543b393b67269d23a988e2827b52e77b5e6c
ReihanFatilla <reihanfatilla@gmail.com>
2023-01-03 19:09:15 +0800

    [feat]: added PreferenceHelper.kt

---
+++ app/src/main/java/com/reift/storyapp/data/local/PreferenceHelper.kt
@@ -0,0 +1,38 @@
+package com.reift.storyapp.data.local
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class PreferenceHelper(val context: Context) {
+
+    var sharedPreferences = context.getSharedPreferences(PREF_USER, 0)
+    val editor: SharedPreferences.Editor = sharedPreferences.edit()
+
+    fun add(key: String, value: String){
+        editor.putString(key, value)
+        editor.apply()
+    }
+
+    fun add(key: String, value: Boolean){
+        editor.putBoolean(key, value)
+        editor.apply()
+    }
+
+    fun getString(key: String): String? {
+        return sharedPreferences.getString(key, "")
+    }
+
+    fun getBoolean(key: String): Boolean {
+        return sharedPreferences.getBoolean(key, false)
+    }
+
+    fun clear(){
+        editor.clear()
+        editor.apply()
+    }
+
+    companion object {
+        const val PREF_USER = "pref_user_database"
+    }
+
+}
