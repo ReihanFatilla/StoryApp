b56679ce554df3b723cd4717489927255ba8814e
ReihanFatilla <reihanfatilla@gmail.com>
2023-01-07 13:44:51 +0800

    [feat]: added post activity ui

--- app/src/main/java/com/reift/storyapp/presentation/post/PostActivity.kt
+++ app/src/main/java/com/reift/storyapp/presentation/post/PostActivity.kt
@@ -3,10 +3,15 @@ package com.reift.storyapp.presentation.post
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.reift.storyapp.R
+import com.reift.storyapp.databinding.ActivityPostBinding
 
 class PostActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPostBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_post)
+        binding = ActivityPostBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
