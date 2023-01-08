ac60754947de274e5f3d96758919c30daea47b73
ReihanFatilla <reihanfatilla@gmail.com>
2023-01-08 14:51:50 +0800

    [feat]: finished login button enablement logic

--- app/src/main/java/com/reift/storyapp/presentation/login/LoginActivity.kt
+++ app/src/main/java/com/reift/storyapp/presentation/login/LoginActivity.kt
@@ -6,10 +6,13 @@ import android.os.Bundle
 import com.reift.storyapp.R
 import com.reift.storyapp.databinding.ActivityLoginBinding
 import com.reift.storyapp.presentation.register.RegisterActivity
+import org.koin.android.viewmodel.ext.android.viewModel
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+
+    private val viewModel: LoginViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +20,14 @@ class LoginActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setUpView()
+        buttonEnablelation()
         validateForm()
+    }
+
+    private fun buttonEnablelation() {
+        viewModel.buttonEnabled.observe(this){
+            binding.btnLogin.isEnabled = it
+        }
     }
 
     private fun setUpView() {
@@ -32,8 +42,12 @@ class LoginActivity : AppCompatActivity() {
 
     private fun validateForm(){
         binding.apply {
-            edtPassword.validateLenght(tilPassword)
-            edtEmail.validateFormat(tilEmail)
+            edtPassword.validateLenght(tilPassword).observe(this@LoginActivity){
+                viewModel.buttonEnabled.value = it == true
+            }
+            edtEmail.validateFormat(tilEmail).observe(this@LoginActivity){
+                viewModel.buttonEnabled.value = it == true
+            }
         }
     }
 }
--- app/src/main/java/com/reift/storyapp/presentation/login/LoginViewModel.kt
+++ app/src/main/java/com/reift/storyapp/presentation/login/LoginViewModel.kt
@@ -1,9 +1,11 @@
 package com.reift.storyapp.presentation.login
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.reift.storyapp.domain.usecase.auth.AuthUseCase
 
 class LoginViewModel(
     val authUseCase: AuthUseCase
 ): ViewModel() {
+    val buttonEnabled = MutableLiveData(false)
 }
