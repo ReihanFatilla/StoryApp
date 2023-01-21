b4896db27c580841389f53475cc40682403f57ce
ReihanFatilla <reihanfatilla@gmail.com>
2023-01-22 06:53:15 +0700

    [feat]: add testing when data is empty

--- app/src/test/java/com/reift/storyapp/presentation/home/HomeViewModelTest.kt
+++ app/src/test/java/com/reift/storyapp/presentation/home/HomeViewModelTest.kt
@@ -2,8 +2,10 @@ package com.reift.storyapp.presentation.home
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.paging.AsyncPagingDataDiffer
+import androidx.paging.PagingData
 import androidx.recyclerview.widget.ListUpdateCallback
 import com.reift.storyapp.data.repository.story.StoryUseCaseRepository
+import com.reift.storyapp.domain.entity.story.Story
 import com.reift.storyapp.presentation.home.adapter.StoryRxAdapter
 import com.reift.storyapp.utils.CoroutinesTestRule
 import com.reift.storyapp.utils.DummyData
@@ -44,7 +46,7 @@ class HomeViewModelTest {
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun `when get story should not return null and return success`() = runTest {
+    fun `when get story should not return null and return success`() {
         val storyPaging = PagedTestDataSource.snapshot(dummyStory)
         val expectedStory = Flowable.just(storyPaging)
         `when`(storyUseCaseRepository.getAllStories()).thenReturn(expectedStory)
@@ -67,10 +69,34 @@ class HomeViewModelTest {
         }
     }
 
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `when Story Empty Should Return No Data`() {
+        val emptyStoryPaging: PagingData<Story> = PagingData.from(emptyList())
+        val expectedQuote = Flowable.just(emptyStoryPaging)
+        `when`(storyUseCaseRepository.getAllStories()).thenReturn(expectedQuote)
+
+        homeViewModel.getAllStories().subscribe { actualStory ->
+            runTest{
+                val differ = AsyncPagingDataDiffer(
+                    diffCallback = StoryRxAdapter.DIFF_CALLBACK,
+                    updateCallback = noopListUpdateCallback,
+                    mainDispatcher = coroutinesTestRule.testDispatcher,
+                    workerDispatcher = coroutinesTestRule.testDispatcher
+                )
+
+                differ.submitData(actualStory)
+
+                assertEquals(0, differ.snapshot().size)
+            }
+        }
+    }
+
     private val noopListUpdateCallback = object : ListUpdateCallback {
         override fun onInserted(position: Int, count: Int) {}
         override fun onRemoved(position: Int, count: Int) {}
         override fun onMoved(fromPosition: Int, toPosition: Int) {}
         override fun onChanged(position: Int, count: Int, payload: Any?) {}
     }
+    
 }
