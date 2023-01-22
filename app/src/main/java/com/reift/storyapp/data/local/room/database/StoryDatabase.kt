8e37e4f0c3ab16faa8189e2e5364ab35de987a7d
ReihanFatilla <reihanfatilla@gmail.com>
2023-01-22 10:17:03 +0700

    [feat]: added remotekeys dao, databas and entity

--- app/src/main/java/com/reift/storyapp/data/local/room/database/StoryDatabase.kt
+++ app/src/main/java/com/reift/storyapp/data/local/room/database/StoryDatabase.kt
@@ -2,10 +2,13 @@ package com.reift.storyapp.data.local.room.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.reift.storyapp.data.local.room.dao.RemoteKeysDao
 import com.reift.storyapp.data.local.room.dao.StoryDao
+import com.reift.storyapp.data.local.room.entity.RemoteKeys
 import com.reift.storyapp.data.local.room.entity.StoryEntity
 
-@Database(entities = [StoryEntity::class], version = 2, exportSchema = false)
+@Database(entities = [StoryEntity::class, RemoteKeys::class], version = 2, exportSchema = false)
 abstract class StoryDatabase: RoomDatabase() {
     abstract fun storyDao(): StoryDao
+    abstract fun remoteKeysDao(): RemoteKeysDao
 }
